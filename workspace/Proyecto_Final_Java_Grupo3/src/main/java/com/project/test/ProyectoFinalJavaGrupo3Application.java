9a772646b4b57b539b952c35d5389508a87969fa
Diego Sperber <diego.sperber@gmail.com>
2022-09-08 12:02:39 +0200

    creo workspace de java

---
+++ workspace/Proyecto_Final_Java_Grupo3/src/main/java/com/project/test/ProyectoFinalJavaGrupo3Application.java
@@ -0,0 +1,13 @@
+package com.project.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalJavaGrupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalJavaGrupo3Application.class, args);
+	}
+
+}
