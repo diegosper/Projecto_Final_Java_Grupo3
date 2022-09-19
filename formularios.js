8acfd7a76c80c75f786e5f0a3d46c843eb58eca7
Jordi-Cordoba <jordimanuelcordoba@gmail.com>
2022-09-19 13:31:22 +0200

    formularios

---
+++ formularios.js
@@ -0,0 +1,7 @@
+let click = () => window.alert();
+
+let formulario = () =>{
+    let formularioC = document.getElementById("#formularioC");
+    formularioC.style.transform = "scale(1)";
+    formularioC.style.opacity = "1";
+  }
